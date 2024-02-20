588c40f4a5208c5bf63fc925cf5f688d844f07e7
lucasmonsan <lucasmonsan@gmail.com>
2024-02-19 22:24:06 -0300

    Global Palette criado e primeiras cores selecionadas

---
+++ global/Palette.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var dark0 = Color("282828")
+var dark1 = Color("3c3836")
+var dark2 = Color("504945")
+
+var light0 = Color("f9f5d7")
+var light1 = Color("ebdbb2")
+var light2 = Color("bdae93")
+
+var red = Color("fb4934")
